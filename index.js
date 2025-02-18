0c3e3c219eb0e8ea560e04d146cf5ae2f7ab6945
ugo635 <ugolechien58901@gmail.com>
2025-02-18 20:43:34 +0100

    Update

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 // Must import other files here for them to work properly 
 // OR 
 // import this file in the other files
-import "./settings.js";
-import "./features/Features.js";
-import "./features/inv.js";
-import "./features/Diana.js";
-import "./features/Stash.js";
-import "./features/FallingBlocks.js";
-import "./features/Mining.js";
-import "./features/Party.js";
+// import "./features/Features.js";
+// import "./features/inv.js";
+// import "./features/Diana.js";
+// import "./features/Stash.js";
+// import "./features/FallingBlocks.js";
+// import "./features/Mining.js";
+// import "./features/Party.js";
+import mmSettingsData from "./settings";
 
 register("gameLoad", () => {
     ChatLib.chat("&6&l[My Module] &r&7Module Loaded");
 });
+
+register("command", () => {
+    mmSettingsData.openGUI()
+}).setName("Mm")
--- settings.js
+++ settings.js
@@ -1,27 +1,75 @@
-import PogObject from "../PogData";
+import { Color } from "../Vigilance";
+import {
+    @ButtonProperty,
+    @CheckboxProperty,
+    Color,
+    @ColorProperty,
+    @PercentSliderProperty,
+    @SelectorProperty,
+    @SwitchProperty,
+    @TextProperty,
+    @SliderProperty,
+    @Vigilant,
+} from 'Vigilance';
+
+
+
+@Vigilant('Mm', 'MyModule', {
+    getCategoryComparator: () => (a, b) => {
+        // By default, categories, subcategories, and properties are sorted alphabetically.
+        // You can override this behavior by returning a negative number if a should be sorted before b,
+        // or a positive number if b should be sorted before a.
+
+        // In this case, we can put Not general! to be above general.
+        const categories = ['General', 'Diana'];
+
+        return categories.indexOf(a.name) - categories.indexOf(b.name);
+    },
+    // getSubcategoryComparator: () => (a, b) => {
+    //     // These function examples will sort the subcategories by the order in the array, so eeeeeee
+    //     // will be above Category.
+    //     const subcategories = ['Burrows', 'Tracker', 'Waypoints', 'Loot Announcer', 'Bobber Counter', 'Other', 'Party Commands'];
+
+    //     return subcategories.indexOf(a.name) - subcategories.indexOf(b.name);
+    // },
+    // getPropertyComparator: () => (a, b) => {
+    //     // And this will put the properties in the order we want them to appear.
+    //     const names = ["Do action!!!", "password", "text", "Color Picker"];
+
+    //     return names.indexOf(a.attributesExt.name) - names.indexOf(b.attributesExt.name);
+    // }
+})
+
+class mmSettingsData {
+    constructor() {
+        this.initialize(this);
+        // this.addDependency("TheNameOfTheThingYouWannaAddDependencyTo", "TheDependency")
+        // this.addDependency("Inq Highlight color", "Inq highlight") for exemple inq highlight color needs inq highlight to be enabled
+    }
+    //----------- General ----------------
+    @SwitchProperty({
+        name: "My Setting",
+        description: "Enable or disable my setting",
+        category: "General",
+        subcategory: "Settings"
+    })
+    MySettingBool = true;
+}
+
+export default new mmSettingsData();
+
 
 // Create the settings object (PogObject automatically loads the JSON file)
-export let mmSettingsData = new PogObject("Mm", {
-    "printContainerClick": 0,
-    "printContainerOpen": 0,
-    "hideFallingBlocks": 0,
-    "replaceAhMsg": 1,
-    "stashMsg": 1,
-    "replaceStashMsg": 1,
-    "vsMatOrItem": "mat",
-}, "MmSettings.json");
+// import PogObject from "../PogData";
+// export let mmSettingsData = new PogObject("Mm", {
+//     "printContainerClick": 0,
+//     "printContainerOpen": 0,
+//     "hideFallingBlocks": 0,
+//     "replaceAhMsg": 1,
+//     "stashMsg": 1,
+//     "replaceStashMsg": 1,
+//     "vsMatOrItem": "mat",
+// }, "MmSettings.json");
 
-
-// Ensure missing values are set but don't overwrite the entire file
-if (mmSettingsData.printContainerClick === undefined) mmSettingsData.printContainerClick = 0;
-if (mmSettingsData.printContainerOpen === undefined) mmSettingsData.printContainerOpen = 0;
-if (mmSettingsData.hideFallingBlocks === undefined) mmSettingsData.hideFallingBlocks = 0;
-if (mmSettingsData.replaceAhMsg === undefined) mmSettingsData.replaceAhMsg = 1;
-if (mmSettingsData.stashMsg === undefined) mmSettingsData.stashMsg = 1;
-if (mmSettingsData.replaceStashMsg === undefined) mmSettingsData.replaceStashMsg = 1;
-if (mmSettingsData.vsMatOrItem === undefined) mmSettingsData.vsMatOrItem = "mat";
-
-
-
-// Save only if new defaults were added
-mmSettingsData.save();
+// // Save only if new defaults were added
+// mmSettingsData.save();
